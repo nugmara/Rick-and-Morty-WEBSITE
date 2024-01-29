4c612893405b35f80432f9ed8166bcbeb971fe85
nugmara <dagmarag0@gmail.com>
2024-01-29 07:55:08 +0100

    Erase everything to start from the beginning

--- src/data/search.js
+++
@@ -1,9 +0,0 @@
-let cards = document.querySelectorAll(".character");
-
-function liveSearch() {
-  let search_query = document.getElementById("searchbox").value;
-
-  for (let i = 0; i < cards.length; i++) {
-    cards[i].textContent.toLowerCase().includes(search_query.toLowerCase());
-  }
-}
